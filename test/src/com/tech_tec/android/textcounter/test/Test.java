197506dbc18426fe022623c62341e7c4e241982b
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-10-14 10:49:25 +0900

    テストプロジェクトを追加
    テスト用のテストクラスを作成

---
+++ test/src/com/tech_tec/android/textcounter/test/Test.java
@@ -0,0 +1,11 @@
+package com.tech_tec.android.textcounter.test;
+
+import android.test.AndroidTestCase;
+
+public class Test extends AndroidTestCase {
+    
+    public void testテスト() {
+        assertTrue(true);
+    }
+    
+}
