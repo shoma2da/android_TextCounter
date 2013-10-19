4a4b482a36a6c2ac1a3308229dbcee8572b5de20
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-10-19 21:53:54 +0900

    サービス停止時に通知を消す実装を追加

--- src/src/com/tech_tec/android/textcounter/service/ClipboardWatchService.java
+++ src/src/com/tech_tec/android/textcounter/service/ClipboardWatchService.java
@@ -7,6 +7,8 @@ import android.os.IBinder;
 import com.tech_tec.android.textcounter.clipboard.ClipboardTextGetter;
 
 public class ClipboardWatchService extends Service {
+    
+    private Notification mNotification;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -18,14 +20,20 @@ public class ClipboardWatchService extends Service {
         super.onCreate();
         
         ClipboardTextGetter getter = new ClipboardTextGetter(this);
-        Notification notification = new Notification(this);
+        mNotification = new Notification(this);
         
         //現在のクリップボード状態を表示
-        notification.show(getter.getTextLength(), getter.getText());
+        mNotification.show(getter.getTextLength(), getter.getText());
         
         //クリップボード変更時の処理を登録
-        OnClipboardChangeListenerImpl listener = new OnClipboardChangeListenerImpl(getter, notification);
+        OnClipboardChangeListenerImpl listener = new OnClipboardChangeListenerImpl(getter, mNotification);
         getter.addOnClipboardChangeListener(listener);
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mNotification.dismiss();
     }
     
     @Override
